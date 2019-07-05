6659badafa8861b819c2f32ea0a2d9cc907582df
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@gmail.com>
2019-07-05 10:02:07 +0530

    insertfirst

--- index.js
+++ index.js
@@ -13,8 +13,15 @@ class LinkedList {
         this.head = null;
         this.size = 0;
     }
+
+    insertFirst(data) {
+        this.head = new Node(data, this.head);
+        this.size++;
+    }
 }
 
-insertFirst(data) {
-    this.head = new Node(data, this.head);
-}
+
+
+const ll = new LinkedList();
+ll.insertFirst(100);
+console.log(ll);
