8c8f6b88466ccc723a729e67da8c13e691035354
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@gmail.com>
2019-07-05 09:48:57 +0530

    insert into LL

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ class Node {
 
 const n1 = new Node(100);
 console.log(n1);
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+}
+
+insertFirst(data) {
+    this.head = new Node(data, this.head);
+}
