e7407ad0915eb9254653563789bfc4a860146aa4
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@gmail.com>
2019-07-05 10:12:28 +0530

    InsertLast

--- index.js
+++ index.js
@@ -16,6 +16,21 @@ class LinkedList {
         this.size++;
     }
 
+    insertLast(data) {
+        let node = new Node(data);
+        let current;
+        if (!this.head) {
+            this.head = node;
+        } else {
+            current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = node;
+        }
+        this.size++;
+    }
+
     printListData() {
         let current = this.head;
         while(current) {
@@ -31,5 +46,6 @@ const ll = new LinkedList();
 ll.insertFirst(100);
 ll.insertFirst(200);
 ll.insertFirst(300);
+ll.insertLast(400);
 //console.log(ll);
 ll.printListData();
