474e7eb5e4b88f9aed4313efda15de1397fac00b
yadnyesh <yadnyesh.juvekar@gmail.com>
2019-12-08 21:20:52 +0530

    Lecture 13

--- xmlhttprequest/app2.js
+++ xmlhttprequest/app2.js
@@ -2,6 +2,23 @@ function requestListener() {
     console.log('this is how the request looks like NOW ', this)
     let data = JSON.parse(this.responseText)
     console.log('the response text now is converted to JSON', data)
+    let userData = data.results[0]
+    console.log('user looks like ', userData)
+    let fname = userData.name.first
+    let lname = userData.name.last
+    let imageSource = userData.picture.large
+
+    let li = document.createElement('li')
+    let img = document.createElement('img')
+    let p = document.createElement('p')
+
+    img.src = imageSource
+    p.innerHTML = `${fname} ${lname}`
+
+    document.getElementById('user').appendChild(li)
+    li.appendChild(img)
+    li.appendChild(p)
+
 }
 
 function requestError() {
